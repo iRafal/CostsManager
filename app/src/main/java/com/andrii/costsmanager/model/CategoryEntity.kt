f19647299369f1235b573bc2789be3ae156ad127
DESKTOP-3DEA3EA\andri <->
2019-08-04 23:47:54 +0300

    Add category entity

---
+++ app/src/main/java/com/andrii/costsmanager/model/CategoryEntity.kt
@@ -0,0 +1,8 @@
+package com.andrii.costsmanager.model
+
+import java.util.*
+
+/**
+ * Created by Andrii Medvid on 8/4/2019.
+ */
+data class CategoryEntity(val id: Long, val name: String, val price: String, val date: Date)
